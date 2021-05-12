ddc448cc339218b74dcfff003648aeb1fca22542
irinam759 <a4759139@gmail.com>
2021-05-12 20:41:53 +0300

    create input and checked list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '', 
+      list:[]
+    }
+  }
+
+  changeText = (event) => {
+ 
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+  addTask = () => {
+    const newTask = {
+      text: this.state.text,
+      isCompleted:false
+    }
+    this.setState({
+      list:this.state.list.concat(newTask),
+      text:''
+    });
+  }
+updateStatus = (index) => {
+  const task = this.state.list[index];
+  task.isCompleted = !task.isCompleted;
+  
 }
 
+  render(){
+    console.log(this.state.list)
+    const allTasks = this.state.list.map((task,index) =>{
+    return (
+    
+    <div key={index}>
+      <input type="checkbox" checked={task.isCompleted} onChange=""/>
+      {task.text}</div>
+    )
+  });
+    return (
+      <div>
+        <input value={this.state.text} onChange={this.changeText} type="text" placeholder="Add new item" ></input>
+        <button type="button" onClick={this.addTask}>Add</button>
+        <div>
+      {allTasks}
+        </div>
+      
+      
+      </div>
+    )
+  }
+}
 export default App;

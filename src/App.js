f8b0184b216b7e770b8629252d21fc096a1a397c
irinam759 <a4759139@gmail.com>
2021-05-13 11:25:05 +0300

    add function update status of list

--- src/App.js
+++ src/App.js
@@ -28,17 +28,24 @@ class App extends React.Component{
   }
 updateStatus = (index) => {
   const task = this.state.list[index];
+  console.log(task)
   task.isCompleted = !task.isCompleted;
-  
+   const list = this.state.list.slice();
+   list.splice(index,1,task);
+   console.log(list);
+  this.setState({
+    list: list
+  })
+ 
 }
 
   render(){
-    console.log(this.state.list)
+    
     const allTasks = this.state.list.map((task,index) =>{
     return (
     
     <div key={index}>
-      <input type="checkbox" checked={task.isCompleted} onChange=""/>
+      <input type="checkbox" checked={task.isCompleted} onChange={(() => this.updateStatus(index))}/>
       {task.text}</div>
     )
   });
